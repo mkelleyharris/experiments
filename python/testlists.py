aca2f536d91dce900bc4766f6444457b6564498e
mkelleyharris <kelley@mkharris.com>
2014-05-05 13:13:45 -0700

    Cleanup per PyLint

--- python/testlists.py
+++ python/testlists.py
@@ -1,43 +1,41 @@
 #!/usr/bin/python
-
-# Tests of lists in Python
-# Goal is learning usage patterns,  and not rigourously testing Python
+"""Tests of lists in Python. Goal is learning, not testing, Python"""
 
 import unittest
 
 
-
-#  Tests follow ----------------------------
-
 class TestLists(unittest.TestCase):
-  def test_simpleList(self):
-    l1 = [1]
-    l1.append(2)
-    self.assertEqual([1, 2], l1)
+    """Unit tests for basic list features"""
 
-  def test_listComprehension1(self):
-    l1 = [ x for x in range(0, 5)]
-    self.assertEqual([0, 1, 2, 3, 4], l1)
-    self.assertEqual([1, 4, 9], [x**2 for x in range(1,4)])
+    def test_simple_list(self):
+        l_1 = [1]
+        l_1.append(2)
+        self.assertEqual([1, 2], l_1)
 
-  def test_sortAscending(self):
-	listOrig = [3, 1, 2]
-	listOrig.sort()
-	self.assertEqual([1, 2, 3], listOrig)
-	
-  def test_sortDescending(self):
-	listOrig = [3, 1, 2]
-	listOrig.sort(reverse=True)
-	self.assertEqual([3, 2, 1], listOrig)
-	
-  def test_sortedAscending(self):
-	listOrig = [3, 1, 2]
-	self.assertEqual([1, 2, 3], sorted(listOrig))
-	
-  def test_sortedDescending(self):
-	listOrig = [3, 1, 2]
-	self.assertEqual([3, 2, 1], sorted(listOrig, reverse=True))
+    def test_list_comprehension(self):
+        l_1 = [x for x in range(0, 5)]
+        self.assertEqual([0, 1, 2, 3, 4], l_1)
+        self.assertEqual([1, 4, 9], [x**2 for x in range(1, 4)])
 
- 
+    def test_sort_ascending(self):
+        list_orig = [3, 1, 2]
+        list_orig.sort()
+        self.assertEqual([1, 2, 3], list_orig)
+
+    def test_sort_descending(self):
+        list_orig = [3, 1, 2]
+        list_orig.sort(reverse=True)
+        self.assertEqual([3, 2, 1], list_orig)
+
+    def test_sorted_ascending(self):
+        list_orig = [3, 1, 2]
+        self.assertEqual([1, 2, 3], sorted(list_orig))
+
+    def test_sorted_descending(self):
+        list_orig = [3, 1, 2]
+        self.assertEqual([3, 2, 1], sorted(list_orig, reverse=True))
+
+
+
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
