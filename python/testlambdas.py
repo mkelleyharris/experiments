6c992ee14fdd6f16d3702277e008cb35701d1f3e
mkelleyharris <kelley@mkharris.com>
2014-05-05 14:21:36 -0700

    cleanup, per PyLint

--- python/testlambdas.py
+++ python/testlambdas.py
@@ -1,44 +1,46 @@
 #!/usr/bin/python
-
-# Tests of lambda functions in Python
-# Goal is learning usage patterns,  and not rigourously testing Python
+"""Tests of lambda functions, for learning, not testing, Python"""
+# Note Lamda's are a bit controversial. Simple functions are often clearer.
 
 import unittest
 
 # define some simple lambda functions
 
-add = lambda x,y : x+y
-mul = lambda x,y : x*y
+ADDIT = lambda x, y: x+y
+MULT = lambda x, y: x*y
 
 # multiple lambda functions with one variable
-multiOp = { 'add':lambda x,y : x+y, \
-            'mul':lambda x,y : x*y  }
+MULTI_OP = {'ADDIT':lambda x, y: x+y, \
+            'MULT':lambda x, y: x*y}
 
 # function that takes a lamba using two variables
-def useLambda2( lam, x, y):
-  return lam(x, y)
+def use_lambda_2(lam, x, y):
+    return lam(x, y)
 
 
 #  Tests follow ----------------------------
 
 class TestLambdas(unittest.TestCase):
-  def test_simpleLambdas(self):
-    self.assertEqual(5, add(2,3))
-    self.assertEqual(8, mul(2,4))
+    """Unit tests for Lambda's"""
 
-  def test_multiOp(self):
-    self.assertEqual(6, multiOp['add'](2,4))
-    self.assertEqual(8, multiOp['mul'](2,4))
+    def test_simple_lambdas(self):
+        self.assertEqual(5, ADDIT(2, 3))
+        self.assertEqual(8, MULT(2, 4))
 
-  def test_function(self):
-    self.assertEqual(5, useLambda2(add, 2,3))
-    self.assertEqual(6, useLambda2(mul, 2,3))
+    def test_multi_op(self):
+        self.assertEqual(6, MULTI_OP['ADDIT'](2, 4))
+        self.assertEqual(8, MULTI_OP['MULT'](2, 4))
 
-  def test_listOperations(self):
-    self.assertEqual([1, 4, 9, 16], map(lambda x:x**2, [1, 2, 3, 4]))
-    list2 = [2,3,4,5]
-    cube=lambda x:x**3
-    self.assertEqual([8, 27, 64, 125], map(cube, list2))
- 
+    def test_function(self):
+        self.assertEqual(5, use_lambda_2(ADDIT, 2, 3))
+        self.assertEqual(6, use_lambda_2(MULT, 2, 3))
+
+    def test_list_operations(self):
+        self.assertEqual([1, 4, 9, 16], map(lambda x: x**2, [1, 2, 3, 4]))
+        list2 = [2, 3, 4, 5]
+        cube = lambda x: x**3
+        self.assertEqual([8, 27, 64, 125], map(cube, list2))
+
+
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
