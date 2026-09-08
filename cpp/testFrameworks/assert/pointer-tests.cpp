6e465ccfab64337ca946227b1a2bf64057910c26
mkelleyharris <kelley@mkharris.com>
2026-09-08 12:50:10 -0600

    move test declarations out of main

--- cpp/testFrameworks/assert/pointer-tests.cpp
+++ cpp/testFrameworks/assert/pointer-tests.cpp
@@ -10,52 +10,56 @@
 #include <assert.h>
 
 
-int main() {
+void testDereferencingPoineterToArray() {
+    int arr[] = {10, 20, 30};
+    int* p = arr;
+    assert( *p == 10 );  //Dereferences p with * accesses first a array element. index 1
+}
 
+void testMixOfRefereningAddOne() {
+    int arr[] = {10, 20, 30};
+    int* p = arr;
+
+    assert( *p + 1 == 11 );  //Dereferencing p with * accesses first array element.
+                // adds 1 to it yielding 11 in a temp context
+    assert( arr[0] == 10 );  // arr index is 0 arr[0] and it's value does not change
+                // precedence on the * as if (*p) + 1
+}
+
+void testIncrementingValue() {
+    int arr[] = {10, 20, 30};
+    int* p = arr;
+    assert( ++*p == 11 ); //precedence equal. Eval right to left
+    assert( arr[0] == 11 ); // array chqnge 10 to 11
+}
+
+void testMixOfRefereningAndIncrerment() {
+    int arr[] = {10, 20, 30};
+    int* p = arr;;
+
+    assert( *p++ == 10); //precedence ++ Eval right to left *(p++)
+    assert( *p == 20 ); 
+}
+
+void testMixOfPreceddence() {
+    int arr[] = {10, 20, 30};
+    int* p = arr;
+    int** q = &p;
+
+    assert( *q == p ); 
+    assert( **q == 10 ); 
+}
+
+
+int main() {
 
     std::cout << "pointer-tests using simple assert function. " << std::endl;  
 
-
-    {
-        int arr[] = {10, 20, 30};
-        int* p = arr;
-        assert( *p == 10 );  //Dereferences p with * accesses first a array element. index 1
-    }
-
-    {
-        int arr[] = {10, 20, 30};
-        int* p = arr;
-
-        assert( *p + 1 == 11 );  //Dereferencing p with * accesses first array element.
-                    // adds 1 to it yielding 11 in a temp context
-        assert( arr[0] == 10 );  // arr index is 0 arr[0] and it's value does not change
-                    // predence on the * as if (*p) + 1
-    }
-
-    {
-        int arr[] = {10, 20, 30};
-        int* p = arr;
-        assert( ++*p == 11 ); //precedence equal. Eval right to left
-        assert( arr[0] == 11 ); // array chqnge 10 to 11
-    }
-
-    {
-        int arr[] = {10, 20, 30};
-        int* p = arr;;
-
-        assert( *p++ == 10); //precedence ++ Eval right to left *(p++)
-        assert( *p == 20 ); 
-    }
-
-    {
-        int arr[] = {10, 20, 30};
-        int* p = arr;
-        int** q = &p;
-
-        assert( *q == p ); 
-        assert( **q == 10 ); 
-    }
-
+    testDereferencingPoineterToArray();
+    testMixOfRefereningAddOne();
+    testIncrementingValue();
+    testMixOfRefereningAndIncrerment();
+    testMixOfPreceddence();
 
     std::cout << "pointer-tests all passed no asserts fired" << std::endl; 
     return 0;
